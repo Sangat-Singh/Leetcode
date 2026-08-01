3c086cfc26869b61c5267b08f07453830c60d478
sangat <144474778+Sangat-Singh@users.noreply.github.com>
2026-08-01 09:47:00 +0530

    Time: 0 ms (100%), Space: 14 MB (56.21%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,23 +1,54 @@
-class Solution {
-public:
-    vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        int n= nums.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            mp[nums[i]]++;
+// class Solution {
+// public:
+//     vector<int> singleNumber(vector<int>& nums) {
+//         unordered_map<int,int>mp;
+//         int n= nums.size();
+//         vector<int>ans;
+//         for(int i=0;i<n;i++)
+//         {
+//             mp[nums[i]]++;
 
-        }
-        for(int num :nums){
-        if(mp[num]==1)
-        {
-            ans.push_back(num);
-        }
-        }
+//         }
+//         for(int num :nums){
+//         if(mp[num]==1)
+//         {
+//             ans.push_back(num);
+//         }
+//         }
 
-        return ans;
+//         return ans;
 
         
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums)
+    {
+       
+        long long xors=0;
+        for(int num : nums)
+        {
+            xors^=num;
+        }
+
+        long long diff=xors &(-xors);
+
+        int a =0;
+        int b=0;
+        for(int num :nums)
+        {
+            if(num & diff)
+            {
+                a^=num;
+            }
+            else
+            {
+                b^=num;
+            }
+        }
+        return{a,b};
     }
-};
+
+    };
