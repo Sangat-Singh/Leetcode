6774cb128347018db532a529ccf90a2f9a14b510
sangat <144474778+Sangat-Singh@users.noreply.github.com>
2026-08-01 19:51:52 +0530

    Time: 0 ms (100%), Space: 21.7 MB (94.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,21 +1,45 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n=nums.size();
+//         unordered_map<int,int>mp;
+//         for(int i=0;i<n;i++)
+//         {
+//             mp[nums[i]]++;
+
+//         }
+//         for(int i=0;i<=nums.size();i++)
+//         {
+//             if(mp.find(i)==mp.end())
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+        
+//     }
+// };
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++)
+        int xor1=0;
+        int xor2=0;
+        for(int num:nums)
         {
-            mp[nums[i]]++;
-
+            xor1^=num;
         }
         for(int i=0;i<=nums.size();i++)
         {
-            if(mp.find(i)==mp.end())
-            {
-                return i;
-            }
+            xor2^=i;
         }
-        return -1;
-        
+
+        return xor1 ^ xor2;
+
+
+
+
+
+
     }
 };
