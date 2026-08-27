73c4072e89a255509fbe299efb9b83e618d5c54e
sangat <144474778+Sangat-Singh@users.noreply.github.com>
2026-08-27 23:20:09 +0530

    Time: 3 ms (33.86%), Space: 11.5 MB (14.83%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,57 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         vector<string>words;
+//         string word="";
+//         for(char ch : s)
+//         {
+//             if(ch !=' ')
+//             {
+//                 word+=ch;
+//             }
+//             else
+//             {
+//                 if(!word.empty())
+//                 {
+//                     words.push_back(word);
+//                     word="";
+//                 }
+//             }
+//         }
+
+//         if(!word.empty())
+//         {
+//             words.push_back(word);
+//         }
+
+//         reverse(words.begin(),words.end());
+
+//         string ans="";
+
+//         for(int i=0;i<words.size();i++)
+//         {
+//             ans+=words[i];
+//             if(i!=words.size()-1)
+//             {
+//                 ans+=" ";
+//             }
+
+//         }
+
+
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
         vector<string>words;
-        string word="";
-        for(char ch : s)
-        {
-            if(ch !=' ')
-            {
-                word+=ch;
-            }
-            else
-            {
-                if(!word.empty())
-                {
-                    words.push_back(word);
-                    word="";
-                }
-            }
-        }
-
-        if(!word.empty())
+        while(ss>> word)
         {
             words.push_back(word);
         }
@@ -28,17 +60,19 @@ public:
 
         string ans="";
 
-        for(int i=0;i<words.size();i++)
+        for(string st: words )
         {
-            ans+=words[i];
-            if(i!=words.size()-1)
+            if(ans.empty())
+            {
+                ans+=st;
+            }
+            else
             {
                 ans+=" ";
+                ans+=st;
             }
-
         }
-
-
         return ans;
+        
     }
 };
